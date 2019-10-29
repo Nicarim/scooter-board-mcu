cde6d7cfbbc758b59a4f974b4695b4f83f00e7df
Marcin Gordziejewski <marcin.gordziejewski@300.codes>
2019-10-29 20:53:59 +0100

    Make it possible to display speed information

--- src/main.cpp
+++ src/main.cpp
@@ -11,27 +11,27 @@ SSD1306Screen *scr;
 bool lcdHelloPrinted = false;
 unsigned long lastSuccess;
 unsigned long startSend;
-bool messageSent = false;
-bool sentGateEnabled = false;
+bool messageSent = true;
 M365UartReciever *uartReciever;
 mijiaCommState g_commState;
 uint8_t packetCursor = 0;
-
+unsigned long timeoutForHello = 3000000;
+String logBuffer;
 ScooterInfo sInfo;
 
 void setup() {
   // put your setup code here, to run once:
   Serial1.begin(115200);
-  pinMode(ENABLE_WRITE_PIN, OUTPUT);
-  lastSuccess = millis();
+
   Serial.begin(192000);
   Serial.println("Starting sniffing");
   uartReciever = new M365UartReciever();
   uartReciever->SetMiSerial(&Serial1);
   uartReciever->SetCommState(&g_commState);
   uartReciever->SetPacketCursor(&packetCursor);
-
   scr = new SSD1306Screen(49, 48);
+  pinMode(ENABLE_WRITE_PIN, OUTPUT);
+  lastSuccess = micros();
 }
 
 void print_packet_to_serial(mijiaPacket *p, char prefix) {
@@ -40,50 +40,14 @@ void print_packet_to_serial(mijiaPacket *p, char prefix) {
 
 void display_debug_screen(mijiaPacket *p) { scr->DisplayDebug(p); }
 
-void display_triggers() { scr->DisplayTriggers(sInfo); }
-
 void loop() {
-  // put your main code here, to run repeatedly:
-  uartReciever->RecieveScooterData();
-  if (uartReciever->HasCompletedData()) {
-    // mijiaPacket *p =
-    //     create_packet_from_array(recievedData, recievedData[2] + 6);
-    mijiaPacket *p = uartReciever->CreatePacketFromRecieved();
-
-    if (!p->validPacket) {
-      print_packet_to_serial(p, 'E');
-    } else if (p->command == 0x01 && p->source == 0x24) {
-      for (int i = 0; i < p->payloadLength; i++) {
-        sInfo.scooterName[i] = (char)p->payloadData[i];
-      }
-    } else if (p->command == 0x01 && p->source == 0x23 && p->argument == 0xB0) {
-      sInfo.speed = (p->payloadData[11] << 8) | p->payloadData[10];
-    } else if (p->command == 0x64 && p->source == 0x20) {
-      // print_packet_to_serial(p, 'D');
-      sInfo.brake = p->payloadData[2];
-      sInfo.thorttle = p->payloadData[1];
-      lastSuccess = millis();
-      messageSent = false;
-      display_triggers();
-    } else {
-      print_packet_to_serial(p, 'D');
-    }
-
-    uartReciever->ResetCommState();
-    packetCursor = 0;
-    delete p;
-  }
-  if (startSend - millis() > 1 && sentGateEnabled) {
-    digitalWrite(ENABLE_WRITE_PIN, LOW);
-    sentGateEnabled = false;
-  }
-  if (lastSuccess - millis() > 3 && !messageSent) {
+  unsigned long startedAt = micros();
+  if ((micros() - lastSuccess) > 2500 && !messageSent) {
     if (Serial1.availableForWrite() > 0) {
       // TODO: This need a refactor, but is a copy for now to test it out
       // uint8_t dataToSend[10] = {0x55, 0xAA, 0x04, 0x22,
       //                           0x01, 0x31, 0x32, 0x00, // data packets
       //                           0x00, 0x00};            // CRC to be fileld
-
       uint8_t dataToSend[12] = {
           0x55,           0xAA,        0x06, 0x20,
           0x61,           0xB0,        0x20, // 5(offset), 6(len)
@@ -98,12 +62,55 @@ void loop() {
       crccalc = crccalc ^ 0xffff;
       dataToSend[10] = (uint8_t)(crccalc & 0xff);
       dataToSend[11] = (uint8_t)((crccalc & 0xff00) >> 8);
+      startSend = micros();
       digitalWrite(ENABLE_WRITE_PIN, HIGH);
-      startSend = millis();
-      sentGateEnabled = true;
       Serial1.write(dataToSend, 12);
-      lastSuccess = millis();
+      // this waits until whole buffer is send out. see
+      // https://forum.arduino.cc/index.php?topic=372226.msg2566999#msg2566999
+      // clang-format off
+      while (!(UCSR1A & (1 << TXC1)));
+      // clang-format on
+      digitalWrite(ENABLE_WRITE_PIN, LOW);
+      lastSuccess = micros();
       messageSent = true;
+      scr->DisplayTriggers(sInfo);
+    }
+  }
+
+  uartReciever->RecieveScooterData();
+  if (uartReciever->HasCompletedData()) {
+    mijiaPacket *p = uartReciever->CreatePacketFromRecieved();
+
+    if (!p->validPacket) {
+      print_packet_to_serial(p, 'E');
+    } else {
+      if (p->command == 0x01 && p->source == 0x24) {
+        for (int i = 0; i < p->payloadLength; i++) {
+          sInfo.scooterName[i] = (char)p->payloadData[i];
+        }
+      } else if (p->command == 0x01 && p->source == 0x23 &&
+                 p->argument == 0xB0) {
+        sInfo.speed = (p->payloadData[11] << 8) | p->payloadData[10];
+      } else if (p->command == 0x64 && p->source == 0x20) {
+        // print_packet_to_serial(p, 'D');
+        sInfo.brake = p->payloadData[2];
+        sInfo.thorttle = p->payloadData[1];
+        lastSuccess = micros();
+        messageSent = false;
+      }
+      // print_packet_to_serial(p, 'D');
+    }
+    uartReciever->ResetCommState();
+    packetCursor = 0;
+    delete p;
+  }
+  unsigned long endedAt = micros();
+  if (endedAt - startedAt > 60) {
+    // Serial.print("Main Recieving and sending takes: ");
+    // Serial.println(endedAt - startedAt);
+    if (logBuffer.length() > 0) {
+      Serial.print(logBuffer);
+      logBuffer = "";
     }
   }
 
